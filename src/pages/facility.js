12c63cc4bb40e7433e45ff2dec19edcf34f7d5e1
Rtlockwood-O <rtlockwoodwork@gmail.com>
2020-11-09 08:27:08 -0700

    testimonials, photos

--- src/pages/facility.js
+++ src/pages/facility.js
@@ -8,13 +8,14 @@ import penlength from '../assets/images/Penlength.jpg'
 import runs from '../assets/images/runs.jpg'
 import wash from '../assets/images/washrack.jpg'
 import haybarn from '../assets/images/haybarn.jpg'
+import cayla from '../assets/images/caylasroundpen.jpg';
 
 const IndexPage = () => (
   <Layout >
     <section className="page-section">
       <div className="container pb-sm-5">
         <div className="row">
-          <div className="col-xs-12 col-md-8">
+          <div className=".col-xs-12 .col-md-8">
             <div className="cta-inner text-center rounded">
               <h2 className="section-heading mb-5">
 
@@ -24,16 +25,12 @@ const IndexPage = () => (
               <ul className="list-unstyled list-hours mb-5 text-left mx-auto">
                 <li className="list-unstyled-item list-hours-item d-flex">
                   150'x 300'  sand arena</li>
-                <img
-                  className="facility-img"
-                  src={arena}
-                  alt=""
-                />
-                <img
-                  className="facility-img"
-                  src={wash}
-                  alt=""
-                />
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={arena}
+                    alt=""
+                  /></div>
                 <li className="list-unstyled-item list-hours-item d-flex">60' sand round pen
                 </li>
                 <li className="list-unstyled-item list-hours-item d-flex">
@@ -42,55 +39,79 @@ const IndexPage = () => (
                 <li className="list-unstyled-item list-hours-item d-flex">Professional lessons and training
                 </li>
 
-
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={jump}
+                    alt=""
+                  />
+                </div>
                 <li className="list-unstyled-item list-hours-item d-flex">
                   Professional lessons and training</li>
                 <li className="list-unstyled-item list-hours-item d-flex">Access to stadium jumps
                 </li>
-                <img
-                  className="facility-img"
-                  src={penlength}
-                  alt=""
-                />
+
+
+
                 <li className="list-unstyled-item list-hours-item d-flex">
                   Outdoor wash rack & Indoor Grooming Area</li>
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={wash}
+                    alt=""
+                  /></div>
                 <li className="list-unstyled-item list-hours-item d-flex">Bathroom for boarders
                 </li>
                 <li className="list-unstyled-item list-hours-item d-flex">
                   Multiple locking tack rooms</li>
                 <li className="list-unstyled-item list-hours-item d-flex">Trailer parking
                 </li>
-                <img
-                  className="facility-img"
-                  src={jump}
-                  alt=""
-                />
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={haybarn}
+                    alt=""
+                  /></div>
                 <li className="list-unstyled-item list-hours-item d-flex">
                   Safe Bayco fencing throughout</li>
                 <li className="list-unstyled-item list-hours-item d-flex">Automatic waterers
                 </li>
+
                 <li className="list-unstyled-item list-hours-item d-flex">
                   Trail riding directly off the property</li>
+
                 <li className="list-unstyled-item list-hours-item d-flex">24-7 free feed hay in pens
                 </li>
-                <img
-                  className="facility-img"
-                  src={runs}
-                  alt=""
-                />
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={runs}
+                    alt=""
+                  /></div>
                 <li className="list-unstyled-item list-hours-item d-flex">
-                  TPartial and full board options</li>
+                  Partial and full board options</li>
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={penlength}
+                    alt=""
+                  />
+                </div>
                 <li className="list-unstyled-item list-hours-item d-flex">Run & group pen boarding options
                 </li>
+                <div className=".col-xs-6">
+                  <img
+                    className="facility-img"
+                    src={cayla}
+                    alt=""
+                  />
+                </div>
               </ul>
-              <img
-                className="facility-img"
-                src={haybarn}
-                alt=""
-              />
             </div>
 
           </div>
+
         </div>
       </div>
     </section>

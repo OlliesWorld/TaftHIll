2f50d97addc742b8d4d9bd2115ad08941854f795
Rtlockwood-O <rtlockwoodwork@gmail.com>
2020-11-14 13:46:34 -0700

    wr logo

--- src/pages/training.js
+++ src/pages/training.js
@@ -6,6 +6,7 @@ import Layout from '../components/Layout';
 
 import cayla from '../assets/images/cayla.jpg';
 import caylacross from '../assets/images/cayla-cross.jpg'
+import wrlogo from '../assets/images/wr_logo_web.png'
 
 const IndexPage = () => (
   <Layout activeLink="training">
@@ -34,6 +35,7 @@ const IndexPage = () => (
               Cayla has a particular love and interest in promoting Mustangs in the english riding disciplines and has become well-known as a Mustang trainer. She is a graduate of the CSU Equine Science program, a two-time finalist in the Extreme Mustang Makeover Competition, an established TIP trainer, and Eventing Trainer. Over the last six years, Cayla has trained or assisted in the training of over 30 Mustangs. This is a huge feat given that she only has one or two in training at any given time! Cayla believes that the more awareness she can bring to the versatility of the breed, the more adoptable these horses will become. Cayla shows her 2016 Mustang Makeover 5th place finisher, Atalanta in Eventing and won 10th place at the 2018 American Eventing Championships. Cayla also won the overall award for best adult pony rider.
               </p>
           </div>
+
         </div>
 
       </div>
@@ -68,7 +70,11 @@ const IndexPage = () => (
               <p className="mb-0 paragraph">
                 Our goal is to help change the story of American Mustangs in the West by providing professional under saddle training and increase adoptions of these amazing horses.
               </p>
-
+              <img
+                className="mx-auto d-flex rounded img-fluid mb-3"
+                src={wrlogo}
+                alt=""
+              />
               <div className="intro-button pt-5">
                 <a className="btn btn-outline-dark btn-xl" href="https://www.wildrosemagic.org/" target="/">
                   More Information

12406731feddb2f4431a76f53f6ef2915377fd56
Rtlockwood-O <rtlockwoodwork@gmail.com>
2020-11-27 15:06:42 -0700

    form css

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -14,7 +14,7 @@ const ThanksPage = () => (
             />
             <h2 className="bg-faded site-header text-center p-3 m-5 rounded">Thank you for sending a message! <br></br><span role="img" aria-label="horse face emoji">🐴</span> We will get back to you as soon as the sun goes down!<span role="img" aria-label="horse face emoji">🐴</span></h2>
             <div class="blue text-center">
-                <Link to="/">Go back to the homepage</Link>
+                <Link to="/">Go Home</Link>
             </div>
         </section>
     </Layout >

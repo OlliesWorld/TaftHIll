23afa178c9ae4fd5206a51e8415729fae49fa397
Rtlockwood-O <rtlockwoodwork@gmail.com>
2021-01-04 20:22:33 -0700

    better analytics setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,28 +8,17 @@ module.exports = {
     siteUrl: `https://www.tafthillacres.com`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    
+   
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-        head: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "GOOGLE_ANALYTICS_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "GOOGLE_ANALYTICS_TRACKING_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "GOOGLE_ANALYTICS_TRACKING_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
+        trackingId: "GOOGLE_ANALYTICS_TRACKING_ID",
+        head: true,
+        anonymize: true,
         
-      }
+      },
     },
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
